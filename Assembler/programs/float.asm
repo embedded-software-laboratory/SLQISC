5505ed4f6b5b9392d657a0a57d54f9a2d8b3acf4
Marcus Völker <voelker@embedded.rwth-aachen.de>
2024-11-13 20:07:26 +0100

    progress on float printing

--- Assembler/programs/float.asm
+++ Assembler/programs/float.asm
@@ -1,16 +1,55 @@
 SECTION main[@0]
   MOV $SP #65280
   MOV $0 ctest
-  CALL sign
-  PRNT $0
-  MOV $0 ctest
-  CALL exp
-  PRNT $0
-  MOV $0 ctest
-  CALL mant
-  PRNT $0
+  CALL fprint
+  OUT @E
+sl:
+  JMP sl
 
 SECTION funcs
+print:
+  PRNT $0
+  RET
+
+itof:
+  RET
+
+fsub:
+  RET
+
+fprint:
+  MOV $1 $0
+  CALL trunc
+  PRNT $0
+  OUT @i
+  CALL itof
+  CALL fsub
+  RET
+
+trunc:
+  MOV $1 $0
+  CALL exp
+  SUB $0 #10
+  MOV $2 $0
+  MOV $0 $1
+  CALL mant
+  ADD $0 #1024
+  JLEQ $2 shr
+shl:
+  MUL $0 #2
+  DEC $2
+  JLEQ $2 exit
+  JMP shl
+shr:
+  NEG $2
+shrloop:
+  JLEQ $2 exit
+  DIV $0 #2
+  DEC $2
+  JMP shrloop
+exit:
+  RET
+
 sign:
   INC $0
   JLEQ $0 signneg
@@ -29,6 +68,11 @@ expneg:
   SUB $0 #32769
 div:
   DIV $0 #1024
+  JLEQ $0 denorm
+  SUB $0 #15
+  RET
+denorm:
+  MOV $0 #-14
   RET
 
 mant:
