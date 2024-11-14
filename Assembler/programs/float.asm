28ff6032005a1ce6f28568c66c6811189d81b99e
Marcus Völker <voelker@embedded.rwth-aachen.de>
2024-11-14 18:50:41 +0100

    float progress (still far from done, probably abandoning for now)

--- Assembler/programs/float.asm
+++ Assembler/programs/float.asm
@@ -1,6 +1,8 @@
 SECTION main[@0]
   MOV $SP #65280
-  MOV $0 ctest
+//  MOV $0 ctest
+  MOV $0 #25
+  CALL itof
   CALL fprint
   OUT @E
 sl:
@@ -12,10 +14,50 @@ print:
   RET
 
 itof:
+  SUB $2 $2
+itofloop:
+  MOV $3 #1024
+  SLQ $0 $3 shifted
+  DEC $2
+  MUL $0 #2
+  JMP itofloop
+shifted:
+  ADD $2 #25
+  MOV $4 #1024
+  MUL $4 $2
+  SUB $0 #1024
+  ADD $0 $4
   RET
 
 fsub:
+  MOV fsubtmp $0
+  CALL exp
+  MOV fsubexpa $0
+  MOV $0 fsubtmp
+  CALL mant
+  MOV fsubmanta $0
+  MOV $0 $1
+  CALL exp
+  MOV fsubexpb $0
+  MOV $0 $1
+  CALL mant
+  MOV fsubmantb $0
+  SUB fsubexpb fsubexpa
+  ADD fsubmantb #1024
+  ADD fsubmanta #1024
+fsubloop:
+  JLEQ fsubexpb mbshifted
+  MUL fsubmantb #2
+  JMP fsubloop
+mbshifted:
+  SUB fsubmantb fsubmanta
   RET
+fsubexpa: 0
+fsubmanta: 0
+fsubexpb: 0
+fsubmantb: 0
+fsubtmp: 0
+
 
 fprint:
   MOV $1 $0
