707ae3543ca14a251d13c4b2d42f92214181afe4
Marcus Völker <voelker@embedded.rwth-aachen.de>
2025-11-13 14:23:12 +0100

    new project for Simulator

---
+++ Simulator/flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "A Nix-flake-based Haskell development environment";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+
+  outputs = inputs:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: inputs.nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import inputs.nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forEachSupportedSystem ({ pkgs }: {
+        default = pkgs.mkShell {
+          name = "slqisc-sim";
+          packages = with pkgs; [
+            cabal-install ghc haskell-language-server stack
+          ];
+        };
+      });
+    };
+}
